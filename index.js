e0bc22af6d7f328059715b45f723ef5a90225dbd
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-10-09 16:18:53 +0600

    get all reviews api added

--- index.js
+++ index.js
@@ -95,6 +95,12 @@ async function run() {
             }
         });
 
+        //get all reviews api
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray();
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
